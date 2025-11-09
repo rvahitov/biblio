59b26e3c8a5377b1734feb66ccc05c807e92f484
Ramil Vakhitov <rvaxitov@outlook.com>
2025-11-09 13:00:27 +0100

    fix code review issues

--- src/Biblio.Citations.Domain/BookDomain/Queries/IBookQuery.cs
+++ src/Biblio.Citations.Domain/BookDomain/Queries/IBookQuery.cs
@@ -3,5 +3,19 @@ namespace Biblio.Citations.Domain.BookDomain.Queries;
 /// <summary>
 /// Marker interface for identifying queries related to books within the Book domain.
 /// Implementations may use this interface for type safety and query dispatching.
+/// 
+/// <example>
+/// This interface can be used with a query dispatcher or mediator pattern. For example:
+/// <code>
+/// public class GetBookByIdQuery : IBookQuery
+/// {
+///     public Guid BookId { get; }
+///     public GetBookByIdQuery(Guid bookId) => BookId = bookId;
+/// }
+/// 
+/// // In a dispatcher or handler:
+/// public Task&lt;Book&gt; Handle(IBookQuery query) { ... }
+/// </code>
+/// </example>
 /// </summary>
 public interface IBookQuery;
