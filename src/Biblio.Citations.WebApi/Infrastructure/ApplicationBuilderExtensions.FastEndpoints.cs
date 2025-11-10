393551728d8d5166cb4e60bb3d96c9f8e0852c50
Ramil Vakhitov <rvaxitov@outlook.com>
2025-11-10 14:51:37 +0100

    create Citation api project Fixes #30

---
+++ src/Biblio.Citations.WebApi/Infrastructure/ApplicationBuilderExtensions.FastEndpoints.cs
@@ -0,0 +1,36 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.Extensions.Hosting;
+
+namespace Biblio.Citations.Infrastructure;
+
+public static partial class ApplicationBuilderExtensions
+{
+    /// <summary>
+    /// Configures FastEndpoints and Swagger for the provided host application builder.
+    /// Adds FastEndpoints services and registers a Swagger document with the API title
+    /// "Biblio Citations API" and version "v1".
+    /// </summary>
+    /// <param name="appBuilder">The host application builder whose services will be configured.</param>
+    /// <returns>The same <see cref="IHostApplicationBuilder"/> instance to allow fluent chaining.</returns>
+    /// <remarks>
+    /// This extension method mutates <see cref="IHostApplicationBuilder.Services"/> by adding
+    /// FastEndpoints and configuring Swagger document settings. Call this during host
+    /// configuration before building the application.
+    /// </remarks>
+    public static IHostApplicationBuilder WithFastEndpoint(this IHostApplicationBuilder appBuilder)
+    {
+        appBuilder.Services
+            .AddFastEndpoints()
+            .SwaggerDocument(swagger =>
+            {
+
+                swagger.DocumentSettings = doc =>
+                {
+                    doc.Title = "Biblio Citations API";
+                    doc.Version = "v1";
+                };
+            });
+        return appBuilder;
+    }
+}
---
+++ src/Biblio.Citations.WebApi/Program.cs
@@ -0,0 +1,32 @@
+
+using Biblio.Citations.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.OpenTelemetry()
+    .CreateBootstrapLogger();
+
+var builder = WebApplication
+    .CreateBuilder(args)
+    .AddServiceDefaults();
+
+builder
+    .WithSerilog() // Configure Serilog for the application
+    .WithFastEndpoint() // Configure FastEndpoints for the application
+    .WithAkka() // Configure Akka.NET for the application
+;
+
+// Add services to the container.
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.Run();
